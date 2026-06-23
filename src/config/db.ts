a1577d831f88d89eb83318e510977041bb485688
Ahamd Hamdan <hamdan.ahmad.lb@gmail.com>
2026-06-24 00:04:34 +0300

    enhance backend starting point

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    const uri = process.env.MONGO_URI as string;
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error("MONGO_URI is not defined in the environment variables");
+    }
     await mongoose.connect(uri);
     console.log("MongoDB connected");
   } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./db";
-
 dotenv.config();
-connectDB();
 
-const app: Application = express();
-const PORT = process.env.PORT || 5000;
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import connectDB from "./config/db";
+async function main() {
+  await connectDB();
 
-app.use(cors());
-app.use(express.json());
+  const app: Application = express();
+  const PORT = process.env.PORT || 5000;
 
-app.get("/api", (req: Request, res: Response) => {
-  res.json({ message: "Backend is running!" });
-});
+  app.use(cors());
+  app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  app.get("/api", (req: Request, res: Response) => {
+    res.json({ message: "Backend is running!" });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+main();
